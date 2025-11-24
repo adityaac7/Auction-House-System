81620c3a6bebc9e8d447f689e457312de1e9b4fc
Sushant Bogati <sushantbogati4@gmail.com>
2025-11-24 15:35:24 -0700

    added a networkclient construcotr  for intializing streams

---
+++ src/common/NetworkClient.java
@@ -0,0 +1,33 @@
+package common;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Utility class for sending and receiving messages over a network connection.
+ * Uses an underlying TCP {@link Socket} and Java object streams to exchange serialized {@code Message} objects.
+ * Thread-safe for concurrent send/receive, with internal synchronization to prevent deadlocks.
+ */
+public class NetworkClient {
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private final Object readLock = new Object();
+    private final Object writeLock = new Object();
+
+    /**
+     * Establishes a client connection to the given host and port,
+     * and initializes the object streams for bidirectional communication.
+     *
+     * @param host remote IP address or hostname
+     * @param port remote TCP port to connect to
+     * @throws IOException if the connection or the streams fail to open
+     */
+    public NetworkClient(String host, int port) throws IOException {
+        this.socket = new Socket(host, port);
+        this.socket.setSoTimeout(30000); // 30-second timeout
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+        this.out.flush();
+        this.in = new ObjectInputStream(socket.getInputStream());
+    }
+}
