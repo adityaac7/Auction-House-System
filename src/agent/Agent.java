08320c3d21e02681b6d65fcd317e970911ea8d9d
Sushant Bogati <sushantbogati4@gmail.com>
2025-11-24 15:49:46 -0700

    Added necesary varriable declarations

--- src/agent/Agent.java
+++ src/agent/Agent.java
@@ -1,7 +1,10 @@
 package agent;
 
 import bank.BankClient;
+import common.AuctionHouseInfo;
 import common.AuctionItem; // If using items
+import common.Message;
+import common.NetworkClient;
 
 import java.io.IOException;
 import java.util.*;
@@ -10,6 +13,25 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Agent {
+    private int accountNumber;
+    private String agentName;
+
+    private NetworkClient bankClient;
+
+    // auctionHouseId -> NetworkClient to that house
+    private Map<Integer, NetworkClient> auctionHouseConnections;
+    // auctionHouseId -> AuctionHouseInfo
+    private Map<Integer, AuctionHouseInfo> auctionHouses;
+    // auctionHouseId -> listener thread
+    private Map<Integer, Thread> listenerThreads;
+
+    // auctionHouseId -> queue of non-notification responses
+    private Map<Integer, BlockingQueue<Message>> responseQueues;
+
+    private volatile double totalBalance;
+    private volatile double availableFunds;
+    private volatile double blockedFunds;
+    private final Object balanceLock = new Object();
 
     public static void main(String[] args) {
         if (args.length < 3) {
