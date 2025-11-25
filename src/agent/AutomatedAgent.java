f0605db7d7227750c57c7b7b5666295405f64eb3
Sushant Bogati <sushantbogati4@gmail.com>
2025-11-25 14:50:27 -0700

    added a constructor for automated agent where i assigned the values for bankhost,ports,names

--- src/agent/AutomatedAgent.java
+++ src/agent/AutomatedAgent.java
@@ -1,5 +1,6 @@
 package agent;
 
+import java.io.IOException;
 import java.util.Random;
 /**
 * Automated agent that randomly bids on items*/
@@ -10,5 +11,16 @@ public class AutomatedAgent {
     private volatile boolean running = false;
     private Random random;
     private long bidInterval;      // Milliseconds between bids
-    private double bidMultiplier;  // How much to increase bid (e.g., 1.1 = 10% increase)
+    private double bidMultiplier;// How much to increase bid (e.g., 1.1 = 10% increase)
+
+    public AutomatedAgent(String agentName, double initialBalance, String bankHost, int bankPort, long bidInterval,
+                          double bidMultiplier) throws IOException, ClassNotFoundException {
+        this.agent = new Agent(agentName, initialBalance, bankHost, bankPort);
+        this.random = new Random();
+        this.bidInterval = bidInterval;
+        this.bidMultiplier = bidMultiplier;
+        System.out.println("[AUTO AGENT] Created: " + agentName);
+        System.out.println("[AUTO AGENT] Bid Interval: " + bidInterval + "ms");
+        System.out.println("[AUTO AGENT] Bid Multiplier: " + bidMultiplier);
+    }
 }
