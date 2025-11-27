49d3c3917777c83890f13167fdeb884f59dd8635
Utshab <utshabun.5@gmail.com>
2025-11-27 15:10:01 -0700

    Created BankClient helper for easy server connection.

--- src/bank/BankClient.java
+++ src/bank/BankClient.java
@@ -1,23 +1,65 @@
 package bank;
+
+import common.NetworkClient;
+import messages.BankMessages;
+import common.Message;
+import common.AuctionHouseInfo;
+
+import java.io.IOException;
+
 /**
  * Helper class to handle raw socket communication with the Bank.
- * Used by both AuctionHouse and Agent.
+ * Used by both AuctionHouse (to register/block funds) and Agent (to register/transfer).
  */
 public class BankClient {
-    // TODO: Implement methods to send strings to Bank and return response
+    private NetworkClient net;
+    private final String host;
+    private final int port;
 
-    public boolean connect(String host, int port) {
-        // TODO
-        return false;
+    public BankClient(String host, int port) {
+        this.host = host;
+        this.port = port;
     }
 
-    public String createAccount(String name) {
-        // TODO
-        return null;
+    /**
+     * Connects to the Bank server.
+     */
+    public void connect() throws IOException {
+        this.net = new NetworkClient(host, port);
     }
 
-    public boolean blockFunds(double amount) {
-        // TODO
-        return false;
+    /**
+     * Sends a request to register an Agent.
+     */
+    public BankMessages.RegisterAgentResponse registerAgent(String name, double balance) throws IOException, ClassNotFoundException {
+        net.sendMessage(new BankMessages.RegisterAgentRequest(name, balance));
+        return (BankMessages.RegisterAgentResponse) net.receiveMessage();
+    }
+
+    /**
+     * Sends a request to register an Auction House.
+     */
+    public BankMessages.RegisterAuctionHouseResponse registerAuctionHouse(String host, int port) throws IOException, ClassNotFoundException {
+        net.sendMessage(new BankMessages.RegisterAuctionHouseRequest(host, port));
+        return (BankMessages.RegisterAuctionHouseResponse) net.receiveMessage();
+    }
+
+    /**
+     * Helper to send a generic request and get a response.
+     * Useful for blocking funds, unblocking, etc.
+     */
+    public Message sendRequest(Message request) throws IOException, ClassNotFoundException {
+        net.sendMessage(request);
+        return net.receiveMessage();
+    }
+
+    public void disconnect() throws IOException {
+        if (net != null) {
+            net.close();
+        }
+    }
+
+    public boolean isConnected() {
+        return net != null && net.isConnected();
     }
 }
