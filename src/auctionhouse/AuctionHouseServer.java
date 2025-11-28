3881ae5934b9cee2be6f62a0a680a8861500eada
pchandara2006 <pchandara2006@unm.edu>
2025-11-27 19:59:42 -0700

    Updated AuctionHouse, handlers, messages, and item info

---
+++ src/auctionhouse/AuctionHouseServer.java
@@ -0,0 +1,10 @@
+package auctionhouse;
+
+/**
+ * It is a wrapper so we can have entry pint for AuctionHouse.
+ */
+public class AuctionHouseServer {
+    public static void main(String[] args) {
+        AuctionHouse.main(args);
+    }
+}
